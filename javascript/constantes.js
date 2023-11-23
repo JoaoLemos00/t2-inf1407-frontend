509aab3ab9fcacb56a56199512381b13787a79b7
JoaoLemos00 <joao.lemos2703@gmail.com>
2023-11-23 17:16:09 -0300

    v1

--- javascript/constantes.js
+++ javascript/constantes.js
@@ -1,4 +1,4 @@
-const backendAddress = 'https://t2-inf1407-backend-b72000e14b2a.herokuapp.com/';
+const backendAddress = 'https://t2-inf1407-backend-c09f78316b39.herokuapp.com/';
 const backendAddress2 = 'http://127.0.0.1:8000/';
 
 const tokenKeyword = 'Token ';
