5e4dc6c82033a6cdc822f3fdd386212b7c319cef
JoaoLemos00 <113486063+JoaoLemos00@users.noreply.github.com>
2023-11-25 17:59:56 -0300

    Update constantes.js

--- javascript/constantes.js
+++ javascript/constantes.js
@@ -1,4 +1,4 @@
-const backendAddress = 'https://t2-inf1407-backend-c09f78316b39.herokuapp.com/';
+const backendAddress = 'https://t2-inf1407-backend2-f5f3c90d2da5.herokuapp.com/';
 const backendAddress2 = 'http://127.0.0.1:8000/';
 
 const tokenKeyword = 'Token ';
